d275d4b1c3980611857affc2849a2e1957f2d75a
IYATT-yx <iyatt@iyatt.com>
2026-04-12 23:27:06 +0800

    增加字符串常量资源定义

--- resource.h
+++ resource.h
@@ -4,21 +4,28 @@
 //
 #define IDS_PROJNAME                    100
 #define IDC_EDIT1                       101
+#define IDS_ShortCommandName            101
 #define IDC_EDIT2                       102
+#define IDS_CommandName                 102
 #define IDD_GENERICPAIREDITDLG          103
+#define IDS_CommandDescription          103
 #define IDC_LABEL1                      104
+#define IDS_CommandGroup                104
 #define IDC_LABEL2                      105
+#define IDS_ChineseProjectName          105
 #define IDC_CHECK1                      106
 #define IDC_CHECK2                      107
 #define IDC_BUTTON1                     108
+#define IDC_LIST1                       109
+#define IDD_MAINBAR                     110
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         32768
-#define _APS_NEXT_CONTROL_VALUE         109
-#define _APS_NEXT_SYMED_VALUE           104
+#define _APS_NEXT_CONTROL_VALUE         110
+#define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
