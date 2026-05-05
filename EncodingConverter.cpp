4acb4abd10147b8d87b41753c11fa3c870e5f241
IYATT-yx <iyatt@iyatt.com>
2026-05-05 21:00:55 +0800

    新增：字符编码转换模块

---
+++ EncodingConverter.cpp
@@ -0,0 +1,55 @@
+﻿/**
+ * @file      EncodingConverter.cpp
+ * @brief     字符编码转换模块的具体实现
+ * @author    IYATT-yx
+ * @copyright Copyright (c) 2026 IYATT-yx.
+ *            Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+module;
+#include <Windows.h>
+
+export module EncodingConverter;
+import std;
+
+export namespace EncodingConverter
+{
+    std::string FromWstringToUtf8(const std::wstring& wstr)
+    {
+        if (wstr.empty())
+        {
+            return std::string();
+        }
+
+        int utf8Len = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+
+        if (utf8Len <= 0)
+        {
+            return std::string();
+        }
+
+        std::string result(static_cast<size_t>(utf8Len) - 1, '\0');
+        WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, result.data(), utf8Len, nullptr, nullptr);
+
+        return result;
+    }
+
+    std::wstring FromUtf8ToWstring(const std::string& utf8Str)
+    {
+        if (utf8Str.empty())
+        {
+            return std::wstring();
+        }
+
+        int wideCharLen = MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), -1, nullptr, 0);
+
+        if (wideCharLen <= 0)
+        {
+            return std::wstring();
+        }
+
+        std::wstring result(static_cast<size_t>(wideCharLen) - 1, L'\0');
+        MultiByteToWideChar(CP_UTF8, 0, utf8Str.c_str(), -1, result.data(), wideCharLen);
+
+        return result;
+    }
+};
---
+++ EncodingConverter.ixx
@@ -0,0 +1,26 @@
+﻿/**
+ * @file      EncodingConverter.ixx
+ * @brief     字符编码转换模块接口声明
+ * @author    IYATT-yx
+ * @copyright Copyright (c) 2026 IYATT-yx.
+ *            Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
+module EncodingConverter;
+import std;
+
+namespace EncodingConverter
+{
+   /**
+    * @brief 将宽字符串转换为 UTF-8 窄字符串。
+    * @param wstr 输入的宽字符串。
+    * @return std::string 转换后的 UTF-8 字节流。
+    */
+    std::string FromWstringToUtf8(const std::wstring& wstr);
+
+   /**
+    * @brief 将 UTF-8 窄字符串转换为宽字符串。
+    * @param utf8Str 输入的 UTF-8 字节流。
+    * @return std::wstring 转换后的宽字符串。
+    */
+    std::wstring FromUtf8ToWstring(const std::string& utf8Str);
+};
