a11f82fe075ff60353f6f8587d65bdcb9f7cbbea
IYATT-yx <iyatt@iyatt.com>
2026-09-25 13:20:55 +0800

    修改注释头

--- DocData.cpp
+++ DocData.cpp
@@ -1,27 +1,10 @@
-﻿// (C) Copyright 2002-2012 by Autodesk, Inc. 
-//
-// Permission to use, copy, modify, and distribute this software in
-// object code form for any purpose and without fee is hereby granted, 
-// provided that the above copyright notice appears in all copies and 
-// that both that copyright notice and the limited warranty and
-// restricted rights notice below appear in all supporting 
-// documentation.
-//
-// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
-// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
-// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
-// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
-// UNINTERRUPTED OR ERROR FREE.
-//
-// Use, duplication, or disclosure by the U.S. Government is subject to 
-// restrictions set forth in FAR 52.227-19 (Commercial Computer
-// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
-// (Rights in Technical Data and Computer Software), as applicable.
-//
-
-//-----------------------------------------------------------------------------
-//----- DocData.cpp : Implementation file
-//-----------------------------------------------------------------------------
+﻿/**
+ * @file      DocData.cpp
+ * @brief     文档相关数据实现
+ * @author    IYATT-yx
+ * @copyright Copyright (c) 2026 IYATT-yx.
+ *            Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
 #include "StdAfx.h"
 
 //-----------------------------------------------------------------------------
